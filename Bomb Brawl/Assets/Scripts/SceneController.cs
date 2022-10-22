0ae982b8700f3aeeacf1a5eb36ab7706b29e8aae
Isaac Taylor <isaac.taylor@ontariotechu.net>
2022-10-22 16:55:25 -0400

    Menu Scene Basics

---
+++ Bomb Brawl/Assets/Scripts/SceneController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance;
+
+    private int currentBuildIndex = 0;
+
+    private void Awake() {
+        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(currentBuildIndex + 1);
+    }
+
+    public void PreviousScene()
+    {
+        SceneManager.LoadScene(currentBuildIndex - 1);
+    }
+
+    public void LoadScene(int sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public int GetCurrentScene()
+    {
+        return SceneManager.GetActiveScene().buildIndex;
+    }
+
+}
