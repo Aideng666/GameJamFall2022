ce1cd5903a7fa2ce65b7b74c37ae2b68b6979d1f
Tyler <tyler.wong@ontariotechu.net>
2022-10-23 14:05:05 -0400

    Replay, Quit, Win Screen added

--- Bomb Brawl/Assets/Scripts/UIManager.cs
+++ Bomb Brawl/Assets/Scripts/UIManager.cs
@@ -14,6 +14,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] Image spaceToStartImage;
     [SerializeField] Image winnerImage;
 
+    [SerializeField] GameObject gameOverPanel;
+
     float pulseDelay = 1;
     float timeToNextPulse = 0;
 
@@ -51,12 +53,10 @@ public class UIManager : MonoBehaviour
 
     public void GameOverScreen(int playerNum)
     {
-        winnerImage.sprite = winnerImages[playerNum];
-
+        gameOverPanel.SetActive(true);
         winnerImage.enabled = true;
-        playAgainImage.enabled = true;
-        spaceToStartImage.enabled = false;
-        //countdownImage.enabled = false;
+        winnerImage.sprite = winnerImages[playerNum];   
+        playAgainImage.enabled = true;   
     }
 
     public void NewGameScreen()
@@ -64,12 +64,10 @@ public class UIManager : MonoBehaviour
         winnerImage.enabled = false;
         playAgainImage.enabled = false;
         spaceToStartImage.enabled = true;
-        //countdownImage.enabled = false;
     }
 
     public void StartCountdown()
     {
-        //countdownImage.enabled = true;
         winnerImage.enabled = false;
         playAgainImage.enabled = false;
         spaceToStartImage.enabled = false;
